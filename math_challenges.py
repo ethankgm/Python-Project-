cacf510e5fa7aaadc34bf4b2f155c59a2c2fca08
ethankangoum <ethan.kangoum@efrei.net>
2024-12-21 11:52:52 +0100

    data added

--- math_challenges.py
+++ math_challenges.py
@@ -9,8 +9,6 @@ def factorial(n):
     else:
         return n * factorial(n - 1)
 
-#def math_challenge_factorial(n):
-#nn  = randit(1, 10)
 def math_challenge_factorial():
     n = randint(1, 10)
     print(f"Math Challenge: Calculate the factorial of {n}.")
@@ -27,3 +25,26 @@ def math_challenge_factorial():
 
 math_challenge_factorial()
 
+def solve_linear_equation():
+        # It generates two random values for a and b between 1 and 10
+        a = randint(1, 10)
+        b = randint(1, 10)
+
+        # We show them the equation
+        print(f"Math Challenge: Solve the linear equation {a}x + {b} = 0.")
+
+        try:
+            # Ask the player for their answer
+            player_answer = float(input("Your answer (value of x): "))
+
+            correct_answer = -b / a
+
+            # Is the answer correct ?
+            if player_answer == correct_answer:
+                print("Correct! You win a key.")
+                return a, b, correct_answer
+            else:
+                print("No, the correct answer is: ", correct_answer)
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+            return False
