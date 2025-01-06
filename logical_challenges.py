f03ec615e24b01e77c4f2691cea3d112d53479a1
ethankangoum <ethan.kangoum@efrei.net>
2025-01-06 11:16:04 +0100

    new thing 3

--- logical_challenges.py
+++ logical_challenges.py
@@ -45,45 +45,8 @@ def nim_game():
 
         player_turn = not player_turn
 
-
 if __name__ == "__main__":
     if nim_game():
         print("Congratulations! You won the game of Nim!")
     else:
         print("Better luck next time. The game master won.")
-
-import json
-
-def treasure_room():
-    with open('TRClues.json') as f:
-        tv_game = json.load(f)
-
-    year = random.choice(list(tv_game.keys()))
-    show = random.choice(tv_game[year]['programs'])
-    clues = tv_game[year]['programs'][show]['clues']
-    code_word = tv_game[year]['programs'][show]['codeword']
-
-    print("Clue 1:", clues[0])
-    print("Clue 2:", clues[1])
-    print("Clue 3:", clues[2])
-
-    attempts = 3
-    answer_correct = False
-
-    while attempts > 0:
-        answer = input("Enter your guess for the code word: ")
-        if answer.lower() == code_word.lower():
-            answer_correct = True
-            break
-        else:
-            attempts -= 1
-            if attempts > 0:
-                print(f"Wrong answer! You have {attempts} attempts left.")
-                print("Additional clue:", clues[3 - attempts])  # Provide an additional clue
-            else:
-                print(f"Sorry, the correct code word was: {code_word}")
-
-    if answer_correct:
-        print("Congratulations! You've unlocked the treasure room!")
-    else:
-        print("Better luck next time!")
